946f245c4df1328aaf35e968390c3bba3c8ad153
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-06-24 14:39:42 +0300

    ex9

--- app.js
+++ app.js
@@ -338,7 +338,7 @@ let commentById = {};
 
 // Complete the exercise in the space below:
     commentById= comments.find(function(comment){
-        return comment.id === "823423"
+        return comment.id === 823423
     }
 
     )
@@ -346,3 +346,23 @@ let commentById = {};
 // Check your work:
 console.log('Exercise 8 my result: ', commentById);
 console.log('Exercise 8 correct result: ', { text: 'Super good', id: 823423 });
+
+/*
+Exercise 9: Array.prototype.findIndex()
+
+Determine the index of the comment that has a specific ID 123523 in an array 
+of comment objects.
+
+- Store the index in the variable 'idx'.
+*/
+
+let idx = null;
+
+// Complete the exercise in the space below:
+    idx=comments.findIndex(function(comment){
+        return comment.id === 123523
+    })
+
+// Check your work:
+console.log('Exercise 9 my result: ', idx);
+console.log('Exercise 9 correct result: ', 3);
